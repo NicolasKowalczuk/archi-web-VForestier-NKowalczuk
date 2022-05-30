a9e772fa4bb58e89deff27ff93529bef78571f4d
NicolasKowalczuk <nicolas.kowalczuk09@gmail.com>
2022-05-30 09:13:47 +0200

    navigation de base

--- projetArchiWeb/src/app/app-routing.module.ts
+++ projetArchiWeb/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EnseignantComponent } from './enseignant/enseignant.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'enseignant', component: EnseignantComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projetArchiWeb/src/app/app.module.ts
+++ projetArchiWeb/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EnseignantComponent } from './enseignant/enseignant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EnseignantComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
